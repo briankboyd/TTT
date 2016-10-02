041217a223b35c5f38cbe64bbe8971338c98705b
ryyaan2004 <ryyaan_2004@yahoo.com>
2016-10-01 21:15:17 -0500

    Updates skeleton

--- src/main/java/tic/tab/toe/SocketServer.java
+++ src/main/java/tic/tab/toe/SocketServer.java
@@ -5,7 +5,30 @@
  *  License, v. 2.0. If a copy of the MPL was not distributed with this
  *  file, You can obtain one at http://mozilla.org/MPL/2.0/.
  */
+package tic.tab.toe;
+
+import javax.websocket.OnClose;
+import javax.websocket.OnMessage;
+import javax.websocket.OnOpen;
+import javax.websocket.Session;
+import javax.websocket.server.ServerEndpoint;
 
 @ServerEndpoint(value = "/", decoders = {}, encoders = {})
 public class SocketServer {
+
+    @OnOpen
+    public void onOpen(Session session)
+    {
+    }
+
+    @OnMessage
+    public void onMessage(Session session)
+    {
+    }
+
+    @OnClose
+    public void onClose(Session session)
+    {
+    }
+
 }
